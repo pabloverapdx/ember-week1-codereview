cade3161947783e162671f0aa7d552f0d7dd8031
Pablo Vera <pablovera.pdx@gmail.com>
2016-06-23 10:19:07 -0700

    add computer property

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   updateQuestionForm: false,
+
+  fullQuestion: Ember.computed('question.content', 'question.author', function() {
+   return this.get('question.content') + ' asked by ' + this.get('question.author');
+  }),
+
   actions: {
     delete(question) {
       if (confirm('Are you sure you want to delete this question?')) {
